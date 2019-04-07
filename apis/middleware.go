406b24cf21f1e6d195bb9b44ff220554cb8c140f
wangzixu <378320002@qq.com>
2019-04-07 21:46:53 +0800

    1, 打开注释, 接口签名验证

--- apis/middleware.go
+++ apis/middleware.go
@@ -1,9 +1,14 @@
 package apis
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 const (
@@ -42,39 +47,39 @@ func BaseVerify() gin.HandlerFunc {
 		fmt.Println("middle BaseVerify", req)
 		m := req.Body
 
-		//以下是验证sign的流程
-		////1,把原始参数map按key排序
-		//params := make([]string, 0, len(m))
-		//for key,_ := range m {
-		//	params = append(params, key)
-		//}
-		//sort.Strings(params)
-		//
-		////2,把原始参数map按一定算法规律生成一个字符串,并md5
-		//var bulder strings.Builder
-		//for _,key := range params {
-		//	v, _ := m[key].(string)
-		//	bulder.WriteString(key)
-		//	bulder.WriteString("=")
-		//	bulder.WriteString(v)
-		//	bulder.WriteString("&")
-		//}
-		//bulder.WriteString(strconv.FormatUint(req.TimeStamp, 10))
-		//bulder.WriteString(SECRET_KEY)
-		//str := bulder.String()
-		////fmt.Println("sign ori : ", str)
-		//
-		////3,MD5指定的字符串
-		//hash := md5.New()
-		//hash.Write([]byte(str))
-		//sign := hex.EncodeToString(hash.Sum(nil))
-		//
-		//fmt.Println("sign =", sign)
-		////4,对比sign
-		//if strings.Compare(sign, req.Sign) != 0 { //sign对不上, 中断请求
-		//	c.AbortWithStatusJSON(http.StatusOK, BaseRes{400, "签名错误!"})
-		//	return
-		//}
+		//以下是验证sign的流程, md5(参数form& + 时间戳 + key)
+		//1,把原始参数map按key排序
+		params := make([]string, 0, len(m))
+		for key, _ := range m {
+			params = append(params, key)
+		}
+		sort.Strings(params)
+
+		//2,把原始参数map按key排序后生成form格式参数  k1=v1&k2=v2&...
+		var bulder = new(strings.Builder)
+		for _, key := range params {
+			v, _ := m[key].(string)
+			bulder.WriteString(key)
+			bulder.WriteString("=")
+			bulder.WriteString(v)
+			bulder.WriteString("&")
+		}
+		bulder.WriteString(strconv.FormatUint(req.TimeStamp, 10))
+		bulder.WriteString(SECRET_KEY)
+		str := bulder.String()
+		//fmt.Println("sign ori : ", str)
+
+		//3,MD5指定的字符串
+		hash := md5.New()
+		hash.Write([]byte(str))
+		sign := hex.EncodeToString(hash.Sum(nil))
+
+		fmt.Println("sign =", sign)
+		//4,对比sign
+		if strings.Compare(sign, req.Sign) != 0 { //sign对不上, 中断请求
+			c.AbortWithStatusJSON(http.StatusOK, BaseRes{400, "签名错误!"})
+			return
+		}
 
 		c.Set(ALL_PARAMES, m)
 		c.Next()
